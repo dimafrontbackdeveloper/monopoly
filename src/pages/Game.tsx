a9579792b0a963fa8b6b6e0404036d683c4dbb08
Dima <dimafrontbackdev@gmail.com>
2022-12-04 17:47:48 +0200

    qwe

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -2,21 +2,29 @@ import React from 'react';
 import { Unity, useUnityContext } from 'react-unity-webgl';
 
 const Game = () => {
-  const { unityProvider } = useUnityContext({
+  const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
     loaderUrl: '/assets/Builds.loader.js',
     dataUrl: '/assets/Builds.data',
     frameworkUrl: '/assets/Builds.framework.js',
     codeUrl: '/assets/Builds.wasm',
   });
 
+  const loadingPercentage = Math.round(loadingProgression * 100);
+
+  console.log(isLoaded);
+  console.log(loadingProgression);
+  console.log(loadingPercentage);
+
   return (
-    <div>
-      <Unity
-        style={{
-          width: '100%',
-        }}
-        unityProvider={unityProvider}
-      />
+    <div className="container">
+      {isLoaded === false && (
+        // We'll conditionally render the loading overlay if the Unity
+        // Application is not loaded.
+        <div className="loading-overlay">
+          <p>Loading... ({loadingPercentage}%)</p>
+        </div>
+      )}
+      <Unity className="unity" style={{ width: '100%' }} unityProvider={unityProvider} />
     </div>
   );
 };
